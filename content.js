6d7c326518a77a26882b5d60e52d21813497c3c7
Sam Goodger <sam.goodger@grow.inc>
2025-01-15 11:57:08 +1100

    fix: styling on other sticky components

--- content.js
+++ content.js
@@ -1,12 +1,15 @@
 const style = document.createElement('style');
 style.textContent = `
-  [data-testid="issue-view-layout-templates-default.ui.foundation-content.foundation-content-wrapper"] {
+  [data-testid="issue-view-layout-templates-default.ui.foundation-content.foundation-content-wrapper"],
+  [data-testid="issue-view-sticky-header-container.sticky-header"] + div {
     position: sticky;
     top: 32px;
     z-index: 98;
   }
   
   [data-testid="issue-view-layout-templates-default.ui.foundation-content.foundation-content-wrapper"]
+  [data-testid="issue-field-summary.ui.issue-field-summary-inline-edit--container"],
+  [data-testid="issue-view-sticky-header-container.sticky-header"] + div
   [data-testid="issue-field-summary.ui.issue-field-summary-inline-edit--container"] {
     background: var(--ds-surface, white);
   }
@@ -19,6 +22,18 @@ style.textContent = `
   [data-testid="issue.views.issue-details.issue-modal.modal-dialog"]
   [data-testid="issue-field-summary.ui.issue-field-summary-inline-edit--container"] {
     background-color: var(--ds-surface-overlay, #FFFFFF);
+  }
+  
+  div:has(> div > [data-testid="issue.activity.comment"]) {
+    top: 76px;
+  }
+  
+  [data-testid="issue.views.issue-details.issue-modal.modal-dialog"] div:has(> div > [data-testid="issue.activity.comment"]) {
+    top: 44px;
+  }
+  
+  [data-testid="issue-view-layout-templates-views.ui.context.visible-hidden.ui.context-items"] summary {
+    z-index: 95;
   }
 `;
 style.id = 'jira-sticky-summary-styles';
